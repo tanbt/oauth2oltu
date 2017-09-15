ded176d7e799a45bfbd6f1b4cd9d31c79df7778e
Tan Bui <trungtanbui@gmail.com>
2017-09-15 17:23:34 +0300

    Fix mysql persistence

--- src/main/java/com/tanbt/oauth2oltu/service/UserService.java
+++ src/main/java/com/tanbt/oauth2oltu/service/UserService.java
@@ -1,20 +1,15 @@
 package com.tanbt.oauth2oltu.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.tanbt.oauth2oltu.entity.User;
 import com.tanbt.oauth2oltu.repository.mysql.MySQLRepository;
 
 public class UserService {
 
+    //todo: error: this is MongoDB repo
     @Autowired
     private MySQLRepository mySQLRepository;
-
-    @Autowired
-    @Qualifier("jdbcTemplate")
-    private JdbcTemplate jdbcTemplate;
 
     /**
      * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
