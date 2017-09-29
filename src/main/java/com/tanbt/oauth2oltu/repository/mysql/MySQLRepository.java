44d4c15ea2819768f583b96f5dc88b4a0b86d5db
Tan Bui <trungtanbui@gmail.com>
2017-09-29 11:12:00 +0300

    Switch to mongodb - spring autowire doesn't support jersey

--- src/main/java/com/tanbt/oauth2oltu/repository/mysql/MySQLRepository.java
+++ src/main/java/com/tanbt/oauth2oltu/repository/mysql/MySQLRepository.java
@@ -1,14 +1,17 @@
-package com.tanbt.oauth2oltu.repository.mysql;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tanbt.oauth2oltu.entity.User;
-
-@Repository
-public interface MySQLRepository extends CrudRepository<User, String> {
-
-    List<User> findByEmailAndPassword(String email, String password);
-}
+//package com.tanbt.oauth2oltu.repository.mysql;
+//
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.tanbt.oauth2oltu.entity.User;
+//
+///**
+// * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.entity-persistence
+// */
+//@Repository
+//public interface MySQLRepository extends CrudRepository<User, String> {
+//
+//    List<User> findByEmailAndPassword(String email, String password);
+//}
--- src/main/java/com/tanbt/oauth2oltu/service/UserService.java
+++ src/main/java/com/tanbt/oauth2oltu/service/UserService.java
@@ -3,27 +3,22 @@ package com.tanbt.oauth2oltu.service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tanbt.oauth2oltu.entity.User;
-import com.tanbt.oauth2oltu.repository.mysql.MySQLRepository;
+import com.tanbt.oauth2oltu.repository.mongo.MyMongoRepository;
 
 public class UserService {
 
-    //todo: error: this is MongoDB repo
     @Autowired
-    private MySQLRepository mySQLRepository;
+    private MyMongoRepository repo;
 
     /**
      * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
-     *
-     * @param email
-     * @param password
-     * @return
      */
     public User getUser(String email, String password) {
-        return mySQLRepository.findByEmailAndPassword(email, password).get(0);
+        return repo.findByEmailAndPassword(email, password).get(0);
     }
 
 
     public User save(User user) {
-        return mySQLRepository.save(user);
+        return repo.save(user);
     }
 }
