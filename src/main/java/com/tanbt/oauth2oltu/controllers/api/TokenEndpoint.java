cedd28e3d063c7e46c806b1d71126f48a1dfdf39
Duc Phan <duclaptop@mgail.com>
2017-10-12 16:16:39 +0300

    Generating access token for 4 grant types

---
+++ src/main/java/com/tanbt/oauth2oltu/controllers/api/TokenEndpoint.java
@@ -0,0 +1,131 @@
+package com.tanbt.oauth2oltu.controllers.api;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
+
+import org.apache.oltu.oauth2.as.issuer.MD5Generator;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
+import org.apache.oltu.oauth2.as.response.OAuthASResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.error.OAuthError;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanbt.oauth2oltu.controllers.api.demo.TestContent;
+import com.tanbt.oauth2oltu.service.UserService;
+
+@RestController
+public class TokenEndpoint {
+
+    @Autowired
+    @Qualifier("userService")
+    UserService userService;
+
+    @RequestMapping(value = "/api/token", method = RequestMethod.POST,
+            consumes = "application/x-www-form-urlencoded", produces = "application/json")
+    public String authorize(@Context HttpServletRequest request)
+            throws OAuthSystemException {
+
+        OAuthTokenRequest oauthRequest = null;
+
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+
+        try {
+            oauthRequest = new OAuthTokenRequest(request);
+
+            //check if client secret is valid
+            if (!TestContent.CLIENT_SECRET.equals(oauthRequest.getParam(OAuth.OAUTH_CLIENT_SECRET))) {
+                OAuthResponse response =
+                        OAuthASResponse.errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                                .setError(OAuthError.TokenResponse.INVALID_CLIENT).setErrorDescription("client_secret invalid")
+                                .buildJSONMessage();
+                return response.getBody();
+            }
+
+            //check if clientid is valid
+            if (!TestContent.CLIENT_ID.equals(oauthRequest.getParam(OAuth.OAUTH_CLIENT_ID))) {
+                OAuthResponse response =
+                    OAuthASResponse.errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                        .setError(OAuthError.TokenResponse.INVALID_CLIENT).setErrorDescription("client_id not found")
+                        .buildJSONMessage();
+                return response.getBody();
+            }
+
+            //do checking for different grant types
+            if (oauthRequest.getParam(OAuth.OAUTH_GRANT_TYPE)
+                .equals(GrantType.AUTHORIZATION_CODE.toString())) {
+                if (!TestContent.AUTHORIZATION_CODE.equals(oauthRequest.getParam(OAuth.OAUTH_CODE))) {
+                    OAuthResponse response = OAuthASResponse
+                        .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                        .setError(OAuthError.TokenResponse.INVALID_GRANT)
+                        .setErrorDescription("invalid authorization code")
+                        .buildJSONMessage();
+                    return response.getBody();
+                }
+            } else if (oauthRequest.getParam(OAuth.OAUTH_GRANT_TYPE)
+                .equals(GrantType.PASSWORD.toString())) {
+                if (!TestContent.PASSWORD.equals(oauthRequest.getPassword())
+                    || !TestContent.USERNAME.equals(oauthRequest.getUsername())) {
+                    OAuthResponse response = OAuthASResponse
+                        .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                        .setError(OAuthError.TokenResponse.INVALID_GRANT)
+                        .setErrorDescription("invalid username or password")
+                        .buildJSONMessage();
+                    return response.getBody();
+                }
+            } else if (oauthRequest.getParam(OAuth.OAUTH_GRANT_TYPE)
+                .equals(GrantType.REFRESH_TOKEN.toString())) {
+                if (!TestContent.REFRESH_TOKEN.equals(oauthRequest
+                        .getRefreshToken())) {
+                    OAuthResponse response = OAuthASResponse
+                            .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                            .setError(OAuthError.TokenResponse.INVALID_GRANT)
+                            .setErrorDescription("invalid refresh token")
+                            .buildJSONMessage();
+                    return response.getBody();
+                }
+            }
+
+            OAuthResponse response = OAuthASResponse
+                .tokenResponse(HttpServletResponse.SC_OK)
+                .setAccessToken(oauthIssuerImpl.accessToken())
+                .setExpiresIn("3600")
+                .setRefreshToken(oauthIssuerImpl.refreshToken())
+                .buildJSONMessage();
+
+            return response.getBody();
+        } catch (OAuthProblemException e) {
+            OAuthResponse res = OAuthASResponse.errorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST).error(e)
+                .buildJSONMessage();
+            return res.getBody();
+        }
+    }
+
+    @RequestMapping(value = "/api/token", method = RequestMethod.GET,
+            consumes = "application/x-www-form-urlencoded", produces = "application/json")
+    public String authorizeGet(@Context HttpServletRequest request) throws
+            OAuthSystemException {
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+
+        OAuthResponse response = OAuthASResponse
+            .tokenResponse(HttpServletResponse.SC_OK)
+            .setAccessToken(oauthIssuerImpl.accessToken())
+            .setExpiresIn("3600")
+            .buildJSONMessage();
+
+        return response.getBody();
+    }
+
+}
